cf97481a60615d8a8fea4961429f605bcf75cdbf
Marc Winter <mwinter@datasolid.de>
2023-08-16 14:11:20 +0200

    Projektdateien hinzufügen.

---
+++ CON_DAL_SQlite/BusinessLogicLayer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace CON_DAL_SQlite
+{
+    public class BusinessLogicLayer
+    {
+        private readonly IPersonRepository _repository;
+
+        public BusinessLogicLayer(IPersonRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public long AddPerson(string name, string vorname, string geburtsdatum, int age, int urlaubstage, string wohnort)
+        {
+            return _repository.CreatePerson(name, vorname, geburtsdatum, age, urlaubstage, wohnort);
+        }
+
+        public List<Person> RetrieveAllPersons()
+        {
+            return _repository.GetAllPersons();
+        }
+
+        public Person RetrievePerson(long personId)
+        {
+            return _repository.GetPerson(personId);
+        }
+
+        public void ModifyPerson(long personId, string? name = null, string? vorname = null, string? geburtsdatum = null, int? age = null, int? urlaubstage = null, string? wohnort = null)
+        {
+            _repository.UpdatePerson(personId, name, vorname, geburtsdatum, age, urlaubstage, wohnort);
+        }
+
+        public void RemovePerson(long personId)
+        {
+            _repository.DeletePerson(personId);
+        }
+    }
+}
---
+++ CON_DAL_SQlite/IPersonRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace CON_DAL_SQlite
+{
+    public interface IPersonRepository
+    {
+        long CreatePerson(string name, string vorname, string geburtsdatum, int age, int urlaubstage, string wohnort);
+
+        public List<Person> GetAllPersons();
+
+        void UpdatePerson(long personId, string name = null, string vorname = null, string geburtsdatum = null, int? age = null, int? urlaubstage = null, string wohnort = null);
+
+        public Person GetPerson(long personId);
+
+        void DeletePerson(long personId);
+    }
+}
